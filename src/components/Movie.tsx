913d2db2425fb5daff0df9b063de236176ab748a
Enrique Piñero <enrique.zh@hotmail.com>
2020-10-02 14:38:44 +0200

    Añadidas fuentes Código formateado Añadido al estado: linksCollapsed y collapsedText Se utiliza reactstrap para los colapsables y los dropdown

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -1,4 +1,14 @@
 import React from "react";
+import '../assets/css/Movie.css' ;
+
+import {
+  Collapse,
+  Button,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 
 interface IMovieProps {
   img: string;
@@ -9,13 +19,15 @@ interface IMovieProps {
   youtubeLinks: string;
   downloadLinks: string[];
   tiers: string[];
-  rating:number;
+  rating: number;
 }
 
 interface IMovieState {
   collapsed: boolean;
   highlighted: boolean;
   hidden: boolean;
+  linksCollapsed:boolean;
+  collapsedText:string;
 }
 
 function vault() {
@@ -67,73 +79,142 @@ function moon() {
 class Movie extends React.Component<IMovieProps, IMovieState> {
   constructor(props: IMovieProps) {
     super(props);
-    this.state = { collapsed: true, highlighted: false, hidden: false };
+    this.state = { collapsed: true, highlighted: false, hidden: false,linksCollapsed:true,collapsedText:"More..."};
   }
-  
+  public toggleCollapse = () => {
+    this.setState({ collapsed: !!!this.state.collapsed , collapsedText:this.state.collapsed?"Less":"More..."});
+    
+  };
+
+  public toggleLinksCollapse = () => {
+    this.setState({ linksCollapsed: !!!this.state.linksCollapsed });
+  };
   public render() {
-    var moonPath = "";
-    var downloadLinksCaptions = ["Dolphin movie (.dtm)","MKV file via BitTorrent (Modern HQ)","MP4 file via BitTorrent (Compatibility)","Mirror archive.org (MKV Modern HQ)","Mirror archive.org (MP4 Compatibility)"];
-    var linksCaptions = ["Submission","Discuss this movie","View Publication"];
+    var downloadLinksCaptions = [
+      "Dolphin movie (.dtm)",
+      "MKV file via BitTorrent (Modern HQ)",
+      "MP4 file via BitTorrent (Compatibility)",
+      "Mirror archive.org (MKV Modern HQ)",
+      "Mirror archive.org (MP4 Compatibility)",
+    ];
+    var linksCaptions = [
+      "Submission",
+      "Discuss this movie",
+      "View Publication",
+    ];
+    var youtubeEmbedLinkBase = "https://www.youtube.com/embed/";
+
     return (
-      <div>
-        <table className="item">
+      <div className="border bg-light border-dark rounded text-dark mb-2">
+        <table className="table">
           <tbody>
             <tr>
-              <td  className="shot" rowSpan={3}>
-                <div>
-                  <img
-                    title=""
-                    alt=""
-                    src={this.props.img}
-                  />
-                  <br></br>
+              <td className="text-justify" rowSpan={2}>
+                <div className="text-center">
+                  {this.props.tiers.includes("vault") ? vault() : ""}
+                  {this.props.tiers.includes("fastest") ? fastest() : ""}
+                  {this.props.tiers.includes("moon") ? moon() : ""}{" "}
+                  <span className="header">
+                    <b>{this.props.title}</b>
+                  </span>
                 </div>
-              </td>
-              <th>
-                {this.props.tiers.includes("vault") ? vault() : ""}
-                {this.props.tiers.includes("fastest") ? fastest() : ""}
-                {this.props.tiers.includes("moon") ? moon() : ""}{" "}
-                <span>{this.props.title}</span>
-              </th>
-            </tr>
-            <tr>
-              <td colSpan={1}>
-                {this.props.downloadLinks.map((link,index)=>{
-                    return <div><a href={link}>{downloadLinksCaptions[index]}<br></br></a></div>
-                })}
+                <div className="container">
+                  <div className="row mt-1">
+                  <iframe
+                  width="560"
+                  height="315"
+                  src={
+                    youtubeEmbedLinkBase + this.props.youtubeLinks.slice(-11)
+                  }
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+                  </div>
+                  <div className="text-right row mt-2">   
+                  <Button
+                    color="primary"
+                    onClick={this.toggleCollapse}
+                    style={{ marginBottom: "1rem" }}
+                  >
+                    {this.state.collapsedText}
+                  </Button>
+                    
+                  </div>
+                </div>
                 
-                <a href={this.props.youtubeLinks}>Watch on (www.youtube.com)</a>
-                <br></br>
-                {this.props.links.map((link,index)=>{
-                    return <a href={link}>{linksCaptions[index]}<br></br></a>
-                })}
-                <span>Rating: {this.props.rating}</span><br></br>
                 
-                {/*    <script type="text/javascript">function rateform_2679() {
+                <Collapse className="body" isOpen={!!!this.state.collapsed}>
+                  <div className="container">
+                    <div className="row">
+                      <div className="col">
+                        {this.props.links.map((link, index) => {
+                          return (
+                            <a href={link}>
+                              {linksCaptions[index]}
+                              <br></br>
+                            </a>
+                          );
+                        })}
+                        <span>Rating: {this.props.rating}</span>
+                        <br></br>
+
+                        <Dropdown isOpen={!!!this.state.linksCollapsed} toggle={this.toggleLinksCollapse} >
+                          <DropdownToggle >Download Links</DropdownToggle>
+                          <DropdownMenu>
+                          {this.props.downloadLinks.map((link, index) => {
+                          return (
+                            <DropdownItem>
+                            <a href={link}>
+                              {downloadLinksCaptions[index]}
+                              <br></br>
+                            </a>
+                            </DropdownItem>
+                          );
+                        })}
+                          </DropdownMenu>
+                        </Dropdown>
+
+                        
+                      </div>
+                      <div className="col">
+                        {this.props.tags.map((tag) => {
+                          return (
+                            <span>
+                              -{tag}
+                              <br></br>
+                            </span>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col">
+                      <div
+                        className="text-justify mt-3"
+                        dangerouslySetInnerHTML={{
+                          __html: this.props.description,
+                        }}
+                      ></div>
+                    </div>
+                  </div>
+                  
+
+                  {/*    <script type="text/javascript">function rateform_2679() {
                             var e = document.getElementById("rateform_2679");
                             e.innerHTML = '<form action="/rating.exe/my/2679" method="post" target="_blank"><tr><th colSpan="3">Your votes:</th></tr><br></br><tr><td className="misc" style="width:30%">The entertainment value of this movie: </td><td className="misc" colSpan="2"><select name="s336fdcf7d540e4b430a890b63da159c9"><option value="-1" selected={true}>-</option><option value="0">0</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option></select>.<select name="d336fdcf7d540e4b430a890b63da159c9"><option value="0" selected={true}>0</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select></td></tr><br></br><tr><td className="misc" style="width:30%">The technical quality of this movie: </td><td className="misc" colSpan="2"><select name="sb3f6d9ddb22b443e40f53e50f0726c7f"><option value="-1" selected={true}>-</option><option value="0">0</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option></select>.<select name="db3f6d9ddb22b443e40f53e50f0726c7f"><option value="0" selected={true}>0</option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select></td></tr></tbody></table><br></br><input type="submit" value="Send data"></form>'
                         }
                     </script>*/}
-                <br></br>
-                
-              </td>
-              <td colSpan={1}>
-                {this.props.tags.map((tag)=>{
-                    return <span>-{tag}<br></br></span>
-                })}
-                
+                  
+                </Collapse>
+                {/* <img title="" alt="" src={this.props.img} /> */}
                 
               </td>
             </tr>
-            <tr>
-              <td colSpan={2} className="blah" style={{ verticalAlign: "top" }}>
-                  <div dangerouslySetInnerHTML={{__html:this.props.description}}></div>
-                
-              </td>
-            </tr>
-            
           </tbody>
         </table>
+        <div></div>
       </div>
     );
   }
